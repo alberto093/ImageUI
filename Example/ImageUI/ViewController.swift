3b3b29bda38185b1fcc0751b317756cfbac6da5f
Alberto Saltarelli <a.saltarelli93@gmail.com>
2020-04-18 17:18:47 +0200

    Add files

---
+++ Example/ImageUI/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ImageUI
+//
+//  Created by alberto093 on 04/12/2020.
+//  Copyright (c) 2020 alberto093. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
