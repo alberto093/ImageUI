a2cf9b9f5b64486718363f426bcda554564d822c
Alberto Saltarelli <a.saltarelli93@gmail.com>
2020-05-09 14:56:00 +0200

    Clean up demo project

--- Demo/Sources/ViewController.swift
+++ Demo/Sources/ViewController.swift
@@ -7,23 +7,23 @@
 //
 
 import UIKit
-//import ImageUI
+import ImageUI
 
 class ViewController: UIViewController {
-//    var browserViewController: IFBrowserViewController {
-//        let images = IFImage.mock
-//        let viewController = IFBrowserViewController(images: images, initialImageIndex: .random(in: images.indices))
-//        viewController.actions = [.share]
-//        return viewController
-//    }
-//
+    var browserViewController: IFBrowserViewController {
+        let images = IFImage.mock
+        let viewController = IFBrowserViewController(images: images, initialImageIndex: .random(in: images.indices))
+        viewController.actions = [.share]
+        return viewController
+    }
+
     @IBAction private func pushButtonDidTap() {
-//        navigationController?.pushViewController(browserViewController, animated: true)
+        navigationController?.pushViewController(browserViewController, animated: true)
     }
     
     @IBAction private func presentButtonDidTap() {
-//        let navigationController = UINavigationController(rootViewController: browserViewController)
-//        navigationController.modalPresentationStyle = .fullScreen
-//        present(navigationController, animated: true)
+        let navigationController = UINavigationController(rootViewController: browserViewController)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true)
     }
 }
